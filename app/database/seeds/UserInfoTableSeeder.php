ec63b5760b4f6e5c1e441eab065c430523602042
Rasel Ahammed <raselcse20@gmail.com>
2017-07-03 01:34:27 +0600

    change

--- app/database/seeds/UserInfoTableSeeder.php
+++ app/database/seeds/UserInfoTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+
 class UserInfoTableSeeder extends Seeder {
 
 	/**
@@ -9,9 +11,11 @@ class UserInfoTableSeeder extends Seeder {
 	 */
 	public function run()
 	{
+		$now = Carbon::now();
+
 		UserModel::insert([
-			['first_name'=>'Rasel', 'last_name'=>'Ahammed', 'user_name'=>'public', 'email'=>'raselcse20@gmail.com', 'password'=>Hash::make('123456'), 'contact'=>'01736803384', 'check_user'=>'no'],
-			['first_name'=>'Rasel', 'last_name'=>'Ahammed', 'user_name'=>'admin', 'email'=>'raselcse20@ymail.com', 'password'=>Hash::make('123456'), 'contact'=>'01521337087', 'check_user'=>'yes']
+			['first_name'=>'Rasel', 'last_name'=>'Ahammed', 'user_name'=>'public', 'email'=>'raselcse20@gmail.com', 'password'=>Hash::make('123456'), 'contact'=>'01736803384', 'check_user'=>'no', 'created_at'=>$now, 'updated_at'=>$now],
+			['first_name'=>'Rasel', 'last_name'=>'Ahammed', 'user_name'=>'admin', 'email'=>'raselcse20@ymail.com', 'password'=>Hash::make('123456'), 'contact'=>'01521337087', 'check_user'=>'yes', 'created_at'=>$now, 'updated_at'=>$now]
 		]);
 	}
 
